abde4b6a732caff17b6769ed39addc03f1246a0f
tilver <10837894+tilver@users.noreply.github.com>
2024-08-18 23:05:48 -0600

    Update simple_uds_example.py; got an answer

--- simple_uds_example.py
+++ simple_uds_example.py
@@ -7,7 +7,7 @@ from scapy.contrib.isotp import *
 
 #socket = CANSocket(channel='vcan0')
 conf.contribs['ISOTP'] = {'use-can-isotp-kernel-module': True}
-sock = ISOTPNativeSocket(iface="vcan0", tx_id=0x610, rx_id=0x6f1, ext_address=0x10, rx_ext_address=0xf1, basecls=UDS)
+sock = ISOTPNativeSocket(iface="vcan0", tx_id=0x7E0, rx_id=0x7e8, basecls=UDS)
 
 uds_request = UDS()/UDS_ER(resetType=0x1)
 sock.sr1(uds_request,verbose=False)
